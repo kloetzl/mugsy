e72da295de0b2f56c56e7394679631486a82d7e9
angiuoli <angiuoli@8aca55ff-8d62-4810-9032-67e444260ce4>
2011-01-22 14:45:48 +0000

    support for genbank file input
    git-svn-id: https://svn.code.sf.net/p/mugsy/code/trunk@14 8aca55ff-8d62-4810-9032-67e444260ce4

--- mugsyenv.sh
+++ mugsyenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export MUGSY_INSTALL=/usr/local/projects/angiuoli/developer/sangiuoli/mugsy/trunk
-export PATH=$PATH:$MUGSY_INSTALL:$MUGSY_INSTALL/mapping
+export MUGSY_INSTALL=/usr/local/projects/angiuoli/mugsy_trunk
+export PATH=$MUGSY_INSTALL:$MUGSY_INSTALL/mapping:$PATH
 export PERL5LIB=$MUGSY_INSTALL/perllibs
 #For testing TBA
 #export PATH=$PATH:$MUGSY_INSTALL/../../multiz-tba/trunk/
